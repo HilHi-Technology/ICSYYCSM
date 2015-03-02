28236b11507d43ecb9dcb1d5e01311329c786bc3
Tin Huynh <tinn_1998@yahoo.com>
2015-03-01 22:12:44 -0800

    Added shadow drawing
    Still bad right now though.

---
+++ Assets/Scripts/DrawScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DrawScript : MonoBehaviour {
+    /*Script placed on the main camera in order to utilize openGL to draw things. Add points to the drawlist to draw quads*/
+    public static List<Vector2> drawList = new List<Vector2>();
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public Material mat;
+    void OnPostRender()
+    {
+        if (!mat)
+        {
+            Debug.LogError("Please Assign a material on the inspector");
+            return;
+        }
+        GL.PushMatrix();
+        mat.SetPass(0);
+        GL.LoadOrtho();
+        GL.Begin(GL.QUADS);
+        GL.Color(Color.black);
+        foreach (Vector2 quad in drawList){
+            GL.Vertex3(Camera.main.WorldToViewportPoint(new Vector3(quad.x, quad.y, 0)).x, Camera.main.WorldToViewportPoint(new Vector3(quad.x, quad.y, 0)).y, 0);
+            //Debug.Log(Camera.main.WorldToViewportPoint(new Vector3(quad.x, quad.y, 0)).x + " " + Camera.main.WorldToViewportPoint(new Vector3(quad.x, quad.y, 0)).y);
+        }
+        GL.End();
+        GL.PopMatrix();
+        drawList = new List<Vector2>(); //reset the draw list
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
     Vector2 playerPos;
+    public LayerMask lightLayerMask;
+    public int rayCastAmnt; //Amount of rays being cast to check for visibility
+    public float visionRange;
+    public float shadowLength;
+    public float shadowOffset;
+
     public float speed; //How fast the player moves
 
     // Use this for initialization
@@ -28,5 +34,56 @@ public class PlayerScript : MonoBehaviour {
         float horMove = Input.GetAxisRaw("Horizontal") * speed;
         float verMove = Input.GetAxisRaw("Vertical") * speed;
         rigidbody2D.velocity = new Vector2(horMove, verMove); //Set the velocity
+
+        //Casting rays and creating dynamic shadows
+        playerPos = new Vector2(transform.position.x, transform.position.y); //Changing player position into a vector2 instead of v3
+        Vector2 prevPoint1 = new Vector2(-9999, -9999); //Previous points are used to keep track of previous ray hits in order to draw quads
+        Vector2 prevPoint2 = new Vector2(-9999, -9999); //Initialized as -9999 because you can't initialize it as null
+        Vector2 startPoint1 = new Vector2(-9999, -9999); //The first ray hit position. Used to connect the last ray hit and the first ray hit together
+        Vector2 startPoint2 = new Vector2(-9999, -9999);
+        for (int i = 0; i < rayCastAmnt; i++) //Iterate through all the rays
+        {
+            float angle = (float)i / (float)rayCastAmnt * 360; //Figure out what angle the ray is in degree.
+            float cos = Mathf.Cos(angle * Mathf.Deg2Rad); //Figure out the cosine of the angle.  Also turn the angle into radians because mathf.sin/cos only take radians
+            float sin = Mathf.Sin(angle * Mathf.Deg2Rad); //Figure out the sine of the angle.
+            Vector2 direction = new Vector2(cos, sin); //Figure out the direction of the raycast (in vector form, e.g. 30deg would be (cos(30),sin(30))).
+            RaycastHit2D rayhit = Physics2D.Raycast(transform.position, direction, visionRange, lightLayerMask); //Cast the ray and store the hit info
+            Vector2 hitPoint;
+            if (rayhit.transform == null) { //check if the ray hits nothing
+                hitPoint = new Vector2(cos * visionRange, sin * visionRange) + playerPos;
+            }
+            else { //If the ray hits something
+                hitPoint = rayhit.point;
+            }
+
+            //Renderer ren = obj.transform.GetComponent<Renderer>();
+            //ren.enabled = true;
+            Vector2 curPoint1 = hitPoint + new Vector2(cos * shadowOffset, sin * shadowOffset); //Ray hit point + shadow offset
+            Vector2 curPoint2 = hitPoint + new Vector2(cos * shadowLength, sin * shadowLength); //Ray hit point + shadow length
+
+            if (prevPoint1 == new Vector2(-9999, -9999) && prevPoint2 == new Vector2(-9999, -9999)) { //If "uninitialized"
+                prevPoint1 = curPoint1;
+                prevPoint2 = curPoint2;
+                startPoint1 = curPoint1; //Set first ray hit point, so we can connect the first and last together later
+                startPoint2 = curPoint2;
+                continue;
+            }
+
+            //Draw the shadows
+            DrawScript.drawList.Add(prevPoint1);
+            DrawScript.drawList.Add(curPoint1);
+            DrawScript.drawList.Add(curPoint2);
+            DrawScript.drawList.Add(prevPoint2);
+
+            prevPoint1 = curPoint1;
+            prevPoint2 = curPoint2;
+
+            if (i == rayCastAmnt - 1) { //If the last ray is casted, draw a shadow connecting the first and last shadow together
+                DrawScript.drawList.Add(startPoint1);
+                DrawScript.drawList.Add(curPoint1);
+                DrawScript.drawList.Add(curPoint2);
+                DrawScript.drawList.Add(startPoint2);
+            }
+        }
     }
 }
