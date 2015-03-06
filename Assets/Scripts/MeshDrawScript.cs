e4103d25cc1d029dcfe770d7399dd1a6ee6386be
Tin Huynh <tinn_1998@yahoo.com>
2015-03-05 21:08:57 -0800

    Warning! Unfinished code!
    Don't touch if you don't know

--- Assets/Scripts/MeshDrawScript.cs
+++ Assets/Scripts/MeshDrawScript.cs
@@ -3,19 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class MeshDrawScript : MonoBehaviour {
-    public static List<Vector2> meshDrawList = new List<Vector2>();
+    public static List<Vector3> meshDrawList = new List<Vector3>();
     private Vector2 previousPoint;
     private List<Vector2> vList = new List<Vector2>();
     private Vector2 mouse_pos;
+    public LayerMask rayMask;
+    public GameObject MeshObj;
+    public List<GameObject> MeshList = new List<GameObject>();
 	// Use this for initialization
 	void Start () {
         //allMeshes = FindObjectsOfType(typeof(PolygonCollider2D)) as PolygonCollider2D[];
         
         previousPoint = transform.position;
+        //Physics2D.raycastsHitTriggers = true;
+        //Physics2D.raycastsStartInColliders = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        foreach (GameObject obj in MeshList) {
+            Destroy(obj);
+        }
+        //mesh.vertices = new Vector3[];
         mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         PolygonCollider2D[] allMeshes = FindObjectsOfType(typeof(PolygonCollider2D)) as PolygonCollider2D[];
@@ -36,9 +45,28 @@ public class MeshDrawScript : MonoBehaviour {
             //Debug.Log(Vector2.Angle(transform.position, v));
             Debug.DrawLine(previousPoint, v, Color.red);
             previousPoint = v;
+            RaycastHit2D ray = Physics2D.Raycast(transform.position, v - (Vector2)transform.position, (v - (Vector2)transform.position).magnitude, rayMask);
+            //Debug.Log(ray.point);
+
+
+
+            if (ray != null) {
+                GameObject obj = Instantiate(MeshObj) as GameObject;
+                MeshList.Add(obj);
+                MeshDraw scr = obj.GetComponent<MeshDraw>();
+                
+                Debug.DrawLine(transform.position, ray.point, Color.white);
+            }
+            else {
+                Debug.DrawLine(transform.position, v, Color.white);
+            }
+
+
+
         }
+
         vList.Clear();
-        Debug.Log(Mathf.Atan2(mouse_pos.x - transform.position.x, mouse_pos.y - transform.position.y) * Mathf.Rad2Deg);
+        //Debug.Log(Mathf.Atan2(mouse_pos.x - transform.position.x, mouse_pos.y - transform.position.y) * Mathf.Rad2Deg);
         
 	}
 }
