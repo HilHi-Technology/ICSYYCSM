fdff9a4250f5a01e95c794d43daecaf2f2f96e45
Tin Huynh <tinn_1998@yahoo.com>
2015-03-12 13:22:21 -0700

    Draws both light and shadow at the same time

--- Assets/Scripts/MeshDraw.cs
+++ Assets/Scripts/MeshDraw.cs
@@ -5,31 +5,24 @@ public class MeshDraw : MonoBehaviour {
     //public Vector3[] vertices;
     //public Vector2[] uv;
 	// Use this for initialization
-    public Vector3[] vertices = new Vector3[4];
-    public Color[] colors = new Color[4];
+    public Vector3[] vertices;
+    public Color[] colors;
     private int i = 1;
+    public bool is_light;
 	void Start () {
         Mesh mesh = GetComponent<MeshFilter>().mesh;
         mesh.Clear();
-        //vertices = new Vector3[3] { new Vector3(0, 0, 0), new Vector3(2, 2, 0), new Vector3(2, 0, 0) };
         mesh.vertices = vertices;
         mesh.colors = colors;
-        //mesh.vertices = new Vector3[] { new Vector3(0, 2, 0), //Top Left
-        //                                new Vector3(2, 2, 0), //Top Right
-        //                                new Vector3(2, 0, 0), //Bot Right
-        //                                                    };//Bot Left
-        //mesh.uv = new Vector2[] { new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1), new Vector2(1, 0)};
-        //if (vertices[0] != null && vertices[1] != null && vertices[2] != null) {
-            mesh.triangles = new int[] { 1, 3, 0, 2, 0 , 3};
-        //}
-        //mesh.colors = new Color[] { new Color(1, 1, 1, 1), new Color(1, 1, 1, 1), new Color(0, 0, 0, 0) };
-        //mesh.colors = new Color[] { new Color(0, 0, 0, 0), new Color(0, 0, 0, 0), new Color(1, 1, 1, 0.5f), new Color(1, 1, 1, 0.5f) };
+        //is_light = false;
+        
+        if (is_light) {
+            mesh.triangles = new int[] { 0, 1, 2 };
+            //Debug.Log(mesh.vertices.Length);
+        } else { 
+            mesh.triangles = new int[] { 1, 3, 0, 2, 0, 3 };
+            //Debug.Log(mesh.vertices.Length);
+        }
+        
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        Debug.Log("Test" + i); //the real comment
-        i++;
-
-	}
-}   
+}
--- Assets/Scripts/MeshDrawScript.cs
+++ Assets/Scripts/MeshDrawScript.cs
@@ -49,10 +49,14 @@ public class MeshDrawScript : MonoBehaviour {
                 RaycastHit2D ray = Physics2D.CircleCast(transform.position, 0.0009f, v - (Vector2)transform.position, 2000, rayMask);
 
                 //Set vertices to be drawn
-                Vector2 prev_point = previousRay.point + (previousPoint - (Vector2)transform.position).normalized * shadow_offset;
-                Vector2 prev_point_extend = previousRay.point + (previousPoint - (Vector2)transform.position).normalized * shadow_length;
-                Vector2 cur_point = ray.point + (v - (Vector2)transform.position).normalized * shadow_offset;
-                Vector2 cur_point_extend = ray.point + (v - (Vector2)transform.position).normalized * shadow_length;
+                Vector2 shadow_prev_point = previousRay.point + (previousPoint - (Vector2)transform.position).normalized * shadow_offset;
+                Vector2 shadow_prev_point_extend = previousRay.point + (previousPoint - (Vector2)transform.position).normalized * shadow_length;
+                Vector2 shadow_cur_point = ray.point + (v - (Vector2)transform.position).normalized * shadow_offset;
+                Vector2 shadow_cur_point_extend = ray.point + (v - (Vector2)transform.position).normalized * shadow_length;
+
+                Vector2 light_prev_point = previousRay.point;
+                Vector2 player_pos = transform.position;
+                Vector2 light_cur_point = ray.point;
 
                 if (previousRay.collider != ray.collider) { //If the 2 vertices are not part of the same object
                     //Shoot another ray to extend the vertex to the next 
@@ -61,30 +65,53 @@ public class MeshDrawScript : MonoBehaviour {
                     RaycastHit2D previousRay2 = Physics2D.Raycast(pStart, previousPoint - (Vector2)transform.position, 2000, rayMask);
                     RaycastHit2D ray2 = Physics2D.Raycast(start, v - (Vector2)transform.position, 2000, rayMask);
 
-                    prev_point = previousRay2.point + (previousPoint - (Vector2)transform.position).normalized * shadow_offset;
-                    prev_point_extend = previousRay2.point + (previousPoint - (Vector2)transform.position).normalized * shadow_length;
-                    cur_point = ray2.point + (v - (Vector2)transform.position).normalized * shadow_offset;
-                    cur_point_extend = ray2.point + (v - (Vector2)transform.position).normalized * shadow_length;
+                    shadow_prev_point = previousRay2.point + (previousPoint - (Vector2)transform.position).normalized * shadow_offset;
+                    shadow_prev_point_extend = previousRay2.point + (previousPoint - (Vector2)transform.position).normalized * shadow_length;
+                    shadow_cur_point = ray2.point + (v - (Vector2)transform.position).normalized * shadow_offset;
+                    shadow_cur_point_extend = ray2.point + (v - (Vector2)transform.position).normalized * shadow_length;
+
+                    light_prev_point = previousRay2.point;
+                    player_pos = transform.position;
+                    light_cur_point = ray2.point;
 
                     Debug.DrawLine(pStart, previousRay2.point, Color.yellow);
                     Debug.DrawLine(start, ray2.point, Color.yellow);
                 }
 
                 //Create draw objects for each light triangles being drawn in order to draw them.
-                GameObject obj = Instantiate(DrawObj, Vector3.zero, Quaternion.identity) as GameObject; 
-                DrawList.Add(obj); //Add draw objects to the this list to be removed eventually
-                MeshDraw scr = obj.GetComponent<MeshDraw>();
-                scr.vertices[0] = (Vector3)prev_point;
-                scr.vertices[1] = (Vector3)prev_point_extend;
-                scr.vertices[2] = (Vector3)cur_point;
-                scr.vertices[3] = (Vector3)cur_point_extend;
+                GameObject shadow_obj = Instantiate(DrawObj, Vector3.zero, Quaternion.identity) as GameObject; 
+                DrawList.Add(shadow_obj); //Add draw objects to the this list to be removed eventually
+                MeshDraw shadow_scr = shadow_obj.GetComponent<MeshDraw>();
+                shadow_scr.vertices = new Vector3[4];
+                shadow_scr.vertices[0] = (Vector3)shadow_prev_point;
+                shadow_scr.vertices[1] = (Vector3)shadow_prev_point_extend;
+                shadow_scr.vertices[2] = (Vector3)shadow_cur_point;
+                shadow_scr.vertices[3] = (Vector3)shadow_cur_point_extend;
 
                 //Set the color of the vision
-                scr.colors[0] = new Color(0, 0, 0, 1f); //Previous point
-                scr.colors[1] = new Color(0, 0, 0, 1f); //This vertex is the player's position
-                scr.colors[2] = new Color(0, 0, 0, 1f); //Current point
-                scr.colors[3] = new Color(0, 0, 0, 1f); //Current point
+                shadow_scr.colors = new Color[4];
+                shadow_scr.colors[0] = new Color(0, 0, 0, 1f); //Previous point
+                shadow_scr.colors[1] = new Color(0, 0, 0, 1f); //This vertex is the player's position
+                shadow_scr.colors[2] = new Color(0, 0, 0, 1f); //Current point
+                shadow_scr.colors[3] = new Color(0, 0, 0, 1f); //Current point
+                shadow_scr.is_light = false;
                 Debug.DrawLine(transform.position, ray.point, Color.white);
+
+                GameObject light_obj = Instantiate(DrawObj, Vector3.zero, Quaternion.identity) as GameObject;
+                DrawList.Add(light_obj); //Add draw objects to the this list to be removed eventually
+                MeshDraw light_scr = light_obj.GetComponent<MeshDraw>();
+
+                light_scr.vertices = new Vector3[3];
+                light_scr.vertices[0] = light_prev_point;
+                light_scr.vertices[1] = player_pos;
+                light_scr.vertices[2] = light_cur_point;
+                //light_scr.vertices[3] = new Vector3(9999,9999,9999); //Generic number that pretty much represents null
+
+                light_scr.colors = new Color[3];
+                light_scr.colors[0] = new Color(1, 1, 1, 0.5f);
+                light_scr.colors[1] = new Color(1, 1, 1, 0.5f); //White
+                light_scr.colors[2] = new Color(1, 1, 1, 0.5f);
+                light_scr.is_light = true;
                 //}
                 
             }
