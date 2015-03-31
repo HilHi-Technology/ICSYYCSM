3e71f6bdef850d6bcfa6265b3937b36e1136ac38
Tin Huynh <tinn_1998@yahoo.com>
2015-03-30 22:16:15 -0700

    Cleaned up several things from the last merge.

--- Assets/Scripts/BadGuyCollider.cs
+++ Assets/Scripts/BadGuyCollider.cs
@@ -14,22 +14,14 @@ public class BadGuyCollider : MonoBehaviour {
 	
 	}
 
-	void OnTriggerEnter2D(Collider2D coll) {               //Calls for update when its run into
-		
-		
-		
+	void OnTriggerEnter2D(Collider2D coll) { 
+        //If player runs into the bad guy, pause the game and display retry buttons
 		if (coll.tag == "Player") {
 			Debug.Log ("Here");
 			GUIText lose = YouLose.GetComponent<GUIText>();  
 			lose.enabled = true;
 			transform.gameObject.AddComponent<GameOverScript>();
 			Time.timeScale = 0;
-			
-
-
-
-			
-			
 		}
 	}
 }
